f2d120f2ea1567cda0758c4d4484575973f62c0e
Tomasz Trybala <tomek.trybala@gmail.com>
2024-07-25 15:02:39 +0200

    Added implementation of match service

--- src/main/java/com/example/sportradardemo/domain/mapper/MatchMapper.java
+++ src/main/java/com/example/sportradardemo/domain/mapper/MatchMapper.java
@@ -4,12 +4,14 @@ import com.example.sportradardemo.domain.model.Match;
 import com.example.sportradardemo.domain.model.MatchScore;
 import com.example.sportradardemo.domain.repository.dto.MatchDTO;
 import lombok.val;
+import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Component
 public class MatchMapper implements Mapper<Match, MatchDTO>{
     @Override
     public Match mapToDomain(MatchDTO dto) {
---
+++ src/main/java/com/example/sportradardemo/domain/service/MatchServiceImpl.java
@@ -0,0 +1,70 @@
+package com.example.sportradardemo.domain.service;
+
+import com.example.sportradardemo.domain.comparator.MatchComparator;
+import com.example.sportradardemo.domain.mapper.MatchMapper;
+import com.example.sportradardemo.domain.model.Match;
+import com.example.sportradardemo.domain.repository.MatchRepository;
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.springframework.stereotype.Service;
+
+import java.time.Clock;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class MatchServiceImpl implements MatchService {
+    private final MatchMapper matchMapper;
+    private final MatchComparator matchComparator;
+    private final MatchRepository matchRepository;
+    private final Clock clock;
+
+    @Override
+    public Match createMatch() {
+        val match = new Match();
+        match.startMatch(clock);
+        matchRepository.createMatch(matchMapper.mapToDTO(match));
+
+        return match;
+    }
+
+    @Override
+    public void homeTeamScored(UUID matchId) {
+        val match = matchRepository.getMatchById(matchId)
+                .map(matchMapper::mapToDomain)
+                .orElseThrow(() -> new IllegalArgumentException("Match not found"));
+
+        match.homeTeamScored();
+        matchRepository.updateMatch(matchMapper.mapToDTO(match));
+    }
+
+    @Override
+    public void awayTeamScored(UUID matchId) {
+        val match = matchRepository.getMatchById(matchId)
+                .map(matchMapper::mapToDomain)
+                .orElseThrow(() -> new IllegalArgumentException("Match not found"));
+
+        match.awayTeamScored();
+        matchRepository.updateMatch(matchMapper.mapToDTO(match));
+    }
+
+    @Override
+    public void finishMatch(UUID matchId) {
+        val match = matchRepository.getMatchById(matchId)
+                .map(matchMapper::mapToDomain)
+                .orElseThrow(() -> new IllegalArgumentException("Match not found"));
+
+        match.endMatch(clock);
+        matchRepository.updateMatch(matchMapper.mapToDTO(match));
+    }
+
+    @Override
+    public List<Match> getScoreboard() {
+        return matchRepository.getAllOngoingMatches()
+                .stream()
+                .map(matchMapper::mapToDomain)
+                .sorted(matchComparator)
+                .toList();
+    }
+}
