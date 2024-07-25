15fc155f0bb187ab0e151a68a011a87cd731efe6
Tomasz Trybala <tomek.trybala@gmail.com>
2024-07-25 12:06:49 +0200

    Initial commit

---
+++ src/main/java/com/example/sportradardemo/SportradarDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportradardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportradarDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportradarDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sportradardemo/SportradarDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportradardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportradarDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
